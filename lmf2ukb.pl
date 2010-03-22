329412ab780f3b709004b608cca93235415b5633
AitorSoroa <AitorSoroa@80d80e37-51c5-447e-889e-3d9dd5ce8e1a>
2010-03-22 16:11:03 +0000

    lmf2ukb changes
    git-svn-id: https://kyoto.let.vu.nl/svn/kyoto/trunk/scripts/kaf_annotate_senses@661 80d80e37-51c5-447e-889e-3d9dd5ce8e1a

--- lmf2ukb.pl
+++ lmf2ukb.pl
@@ -11,13 +11,14 @@ use Getopt::Std;
 
 my %opts;
 
-getopts('', \%opts);
+getopts('gde', \%opts);
 
-die "usage $0 [-g] [-d] wn_lmf.xml\n\tRead from wn_lmf.xml. Create wn_lmf_dict.txt and wn_lmf_links.txt\nOptions:\t-d create just the dict (no graph).\n\t\t-g create just the graph (no dict).\n"
+die "usage $0 [-g] [-d] wn_lmf.xml\n\tRead from wn_lmf.xml. Create wn_lmf_dict.txt and wn_lmf_links.txt\nOptions:\t-d create just the dict (no graph).\n\t\t-g create just the graph (no dict).\n\t\t-e create also external references file.\n"
   unless @ARGV;
 
 my $opt_g = 1;
 my $opt_d = 1;
+my $opt_e = 0;
 
 if (defined $opts{'d'}) {
   $opt_g = 0;
@@ -27,12 +28,17 @@ if (defined $opts{'g'}) {
   $opt_d = 0;
 }
 
+if (defined $opts{'e'}) {
+  $opt_e = 1;
+}
+
 die "You can't specify both -g and -d!\n" unless $opt_g || $opt_d;
 
 my ($wn_name, $dir) = fileparse($ARGV[0], qw/.xml .XML/);
 
-my $dict_fname = "$dir/$wn_name"."_dict.txt";
-my $graph_fname = "$dir/$wn_name"."_links.txt";
+my $dict_fname = "$wn_name"."_dict.txt";
+my $graph_fname = "$wn_name"."_links.txt";
+my $xref_fname = "$wn_name"."_xref.txt";
 
 my $parser = XML::LibXML->new();
 my $doc = $parser->parse_file($ARGV[0]);
@@ -41,10 +47,15 @@ my $root = $doc->getDocumentElement;
 my %D;
 
 my $graph_fh;
+
+my ($lexicon_elem) = $root->findnodes("//Lexicon");
+my $src = $lexicon_elem->getAttribute("label");
+$src =~ s/\s+/_/g;
+
 if ($opt_g) {
   open($graph_fh, ">$graph_fname") || die "Can't create $graph_fname:$!\n";
   binmode($graph_fh, ':utf8');
-  &fill_graph($root, $graph_fh);
+  &fill_graph($root, $graph_fh, $src);
 }
 
 if($opt_d) {
@@ -52,6 +63,12 @@ if($opt_d) {
   open(my $dict_fh, ">$dict_fname") || die "Can't create $dict_fname:$!\n";
   binmode($dict_fh, ':utf8');
   &fill_dict($root, $dict_fh);
+}
+
+if($opt_e) {
+  open(my $xref_fh, ">$xref_fname") || die "Can't create $dict_fname:$!\n";
+  binmode($xref_fh, ':utf8');
+  &fill_xref($root, $xref_fh, "${src}_xref");
 }
 
 
@@ -67,14 +84,15 @@ sub fill_dict {
       print STDERR "No lemma in LexicalEntry $le_id\n";
       next;
     }
-    foreach my $sense_elem ($le_elem->findnodes("./Sense")) {
+    my @Sense_elems = $le_elem->findnodes("./Sense");
+    print STDERR "No senses in LexicalEntry $le_id\n" unless @Sense_elems;
+    foreach my $sense_elem (@Sense_elems) {
       my $sset = $sense_elem->getAttribute("synset");
       my $pos = (split(/\-/, $sset))[-1];
       # update the dictionary
       push @{ $D{"$lemma"}->{$pos} }, $sset;
     }
   }
-
 
   while (my ($k, $v) = each %D) {
     print $d_fh $k;
@@ -88,10 +106,7 @@ sub fill_dict {
 
 sub fill_graph {
 
-  my($root, $g_fh) = @_;
-
-  my ($lexicon_elem) = $root->findnodes("//Lexicon");
-  my $src = $lexicon_elem->getAttribute("label");
+  my($root, $g_fh, $src) = @_;
 
   foreach my $synset_elem ($lexicon_elem->findnodes("Synset")) {
     my $u_id = $synset_elem->getAttribute("id");
@@ -99,6 +114,7 @@ sub fill_graph {
     foreach my $rel_elem ($synset_elem->findnodes("SynsetRelations/SynsetRelation")) {
       my $v_id = $rel_elem->getAttribute("target");
       next unless $v_id;
+      next if ($u_id eq $v_id);
       print $g_fh "u:$u_id v:$v_id";
       print $g_fh " s:$src" if $src;
       print $g_fh "\n";
@@ -106,3 +122,17 @@ sub fill_graph {
   }
 }
 
+sub fill_xref {
+
+  my($root, $g_fh, $src) = @_;
+
+  foreach my $saxis_elem ($root->findnodes("//SenseAxis")) {
+    my ($u_id, $v_id) = map { $_->getAttribute("ID") } $saxis_elem->findnodes("./Target");
+    next unless $u_id;
+    next unless $v_id;
+    next if ($u_id eq $v_id);
+    print $g_fh "u:$u_id v:$v_id";
+    print $g_fh " s:$src" if $src;
+    print $g_fh "\n";
+  }
+}
